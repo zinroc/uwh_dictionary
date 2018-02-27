a3c32d16ebfa2c6c10562d345a63fbc6412c34bc
zinroc <sergei1191@hotmail.com>
2018-02-26 19:47:49 -0500

    clickable phases

--- public/js/home_ctrl.js
+++ public/js/home_ctrl.js
@@ -12,10 +12,10 @@ var getCookie = function (key) {
     }
 
 var Phase_Options = [
-    {name: "Wall_Corner", x: 25, y: 25, width:5, height: 5},
-    {name: "Puck_Collection", x: 0, y: 0, width: 5, height: 5},
-    {name: "Puck_Distribution", x: 0, y: 0, width: 5, height: 5},
-    {name: "Formation_Displacement", x: 0, y: 0, width: 5, height: 5}
+    {name: "Wall_Corner", x: 20.7, y: 24, width:12.2, height: 9, el: null},
+    {name: "Puck_Collection", x: 75, y: 47.2, width: 12, height: 12, el: null},
+    {name: "Puck_Distribution", x: 64.2, y: 79.5, width: 11.2, height: 10.7, el: null},
+    {name: "Formation_Displacement", x: 86, y: 79.5, width: 11.2, height: 11, el: null}
 ];
 
 app.controller("HomeCtrl", function homeCtrl ($scope, api_service) {
@@ -26,13 +26,21 @@ app.controller("HomeCtrl", function homeCtrl ($scope, api_service) {
     $scope.populatePhaseButtons = function () {
         Phase_Options.forEach(function(phase) {
             console.log(phase);
-            $($scope.UIButton(phase.name, phase.x, phase.y, phase.width, phase.height)).appendTo("#top-panel");
+            var el = $($scope.UIButton(phase.name, phase.x, phase.y, phase.width, phase.height)).appendTo("#top-panel");
+
+            $(el).click(function() {$scope.selectPhase(phase.name)});
         });
     }
 
-
     $scope.UIButton = function (name, left, top, width, height) {
-        return '<div id="'+ name +'" class="phase-button" style="position: absolute; left: '+ left +'%; top: '+ top +'%; width: '+ width +'%; height: '+ height +'%"></div>';
+        let s1 = '<div id="'+ name +'" class="phase-button" style="position: absolute; left: '+ left +'%; top: ';
+        let s2 = top +'%; width: '+ width +'%; height: '+ height +'%"';
+        let s3 = '></div>';
+        return s1 + s2 + s3;
+    }
+
+    $scope.selectPhase = function(phase) {
+        console.log(phase);
     }
 
     $scope.populatePhaseButtons();
